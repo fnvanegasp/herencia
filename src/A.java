6bd9c2822958e516cd267376cabcdb5c5ac05a25
Estudiante <Estudiante@FI454402-34.unal.loc>
2016-10-11 15:47:03 -0500

    herencia

---
+++ src/A.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Estudiante
+ */
+public class A {
+    protected int x;
+    
+    public A(int x) {
+    this.x = x;
+    }
+    
+    protected void yy(){
+        System.out.println("yy");
+    }
+    
+}
---
+++ src/B.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Estudiante
+ */
+public class B extends A{
+    public int y;
+    
+    
+    @Override
+    public void yy(){
+        System.out.println("xx" + x);
+    }
+
+    public B(int y, int x) {
+        super(x);
+        this.y = y;
+    }
+    
+    
+    
+}
